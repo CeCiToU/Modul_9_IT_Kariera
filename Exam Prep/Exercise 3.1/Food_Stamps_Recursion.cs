33a2afbd3e01c1864c559766ff74f65ddb3e2489
Tsvetoslav <cveti3060@gmail.com>
2023-10-13 23:11:01 +0300

    Exam exercises

---
+++ Exam Prep/Exercise 3.1/Food_Stamps_Recursion.cs
@@ -0,0 +1,43 @@
+﻿namespace Food_Stamps
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int workedDays = int.Parse(Console.ReadLine());
+
+            if (1 <= workedDays && workedDays <= 23)
+            {
+                Print(CalculateFoodStamps(StampSum(workedDays)));
+            }
+        }
+
+        private static void Print(int stamps)
+        {
+            Console.WriteLine(stamps);
+        }
+
+        private static int StampSum(int days)
+        {
+            return days * 20;
+        }
+
+        private static int CalculateFoodStamps(int stampSum)
+        {
+            int numberOfStamps = 0;
+
+            if (stampSum >= 100)
+            {
+                numberOfStamps += CalculateFoodStamps(stampSum - 100);
+                return numberOfStamps + 1;
+            }
+            else if (stampSum >= 20)
+            {
+                numberOfStamps += CalculateFoodStamps(stampSum - 20);
+                return numberOfStamps + 1;
+            }
+
+            return numberOfStamps;
+        }
+    }
+}
---
+++ Exam Prep/Exercise 3.2/Mult_Recursion.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Mult
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+
+            int mult = Mult(n, m);
+
+            Console.WriteLine(mult);
+        }
+
+        private static int Mult(int n, int m)
+        {
+            int mult = m;
+
+            if (n > 1)
+            {
+                return mult + Mult(n - 1, m);
+            }
+
+            return mult;
+        }
+    }
+}
